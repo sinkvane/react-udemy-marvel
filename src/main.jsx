2f1720bc5775d512b47e5ed861b8fe0ac34400d3
Sinkvane <sinkvadev@gmail.com>
2024-06-23 16:49:13 +0700

    character random loading added & more optimization

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/App';
-import MarvelService from "./services/MarvelService";
-import './style/style.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/app/App";
+import "./style/style.scss";
 
-const marvelService = new MarvelService();
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-marvelService.getAllCharacters().then((res) => res.data.results.forEach((item) => console.log(item.name)));
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 );
-
